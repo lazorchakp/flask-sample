b243c6f9f391f6b472f90bf3e1b398d96ee13743
sugtar <sugtar@gmail.com>
2023-01-03 17:16:17 +0900

    set flask port 8000

--- post-deploy.sh
+++ post-deploy.sh
@@ -4,4 +4,4 @@ apt-get install -y gcc make automake autoconf python3-dev python3-pip
 pip install -r /home/site/wwwroot/requirements.txt
 which flask
 cd /home/site/wwwroot
-flask --app app run
+flask --app app run --port 8000
