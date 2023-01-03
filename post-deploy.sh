262c238d58fa31508cd29e9e6c7b3cbd7f555b4e
sugtar <sugtar@gmail.com>
2023-01-03 15:48:46 +0900

    correct start-up script

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-apt-get update
 apt-get install -y gcc make automake autoconf python3-dev python3-pip
-pip -r /home/site/wwwroot/requirements.txt
+pip install -r /home/site/wwwroot/requirements.txt
