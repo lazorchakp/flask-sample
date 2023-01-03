8d32698d01d6286dff6a6af5da58ea9cbf3f4d79
sugtar <sugtar@gmail.com>
2023-01-03 14:44:45 +0900

    Add Contrast Agent

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from contrast.flask import ContrastMiddleware
+
 app = Flask(__name__)
+app.wsgi_app = ContrastMiddleware(app)
 
 @app.route('/')
 def hello():
-    name = "Hello New World"
+    name = "Hello New World with Contrast"
     return name
 
 if __name__ == "__main__":
